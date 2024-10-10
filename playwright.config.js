56b9635fa90d8de12dc8e1b5b5f399f1618c0dec
DESKTOP\Juciele <juhh.131@gmail.com>
2024-10-09 21:36:26 -0300

    Ajustado arquivo de configuração conforme prompt gerado pelo comando da apresentação

--- playwright.config.js
+++ playwright.config.js
@@ -1,30 +1,20 @@
-// playwright.config.js
 const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
   use: {
-    baseURL: 'http://165.227.93.41/lojinha-web/v2/',
-    headless: false, // Altere para false se quiser ver o navegador aberto durante os testes
-    viewport: { width: 1280, height: 720 },
-    screenshot: 'on', // Captura screenshots em caso de falha
-    video: 'retain-on-failure', // Grava um vídeo apenas quando os testes falharem
+    baseURL: 'http://165.227.93.41/lojinha-web/v2/', // URL base para seus testes
+    headless: false,  // para rodar os testes em um navegador visível
+    viewport: { width: 1280, height: 720 },  // tamanho da tela
+    screenshot: 'only-on-failure',  // tirar screenshots somente em falhas
+    video: 'retain-on-failure',  // gravar vídeos das sessões somente em falhas
   },
-  // Configurações globais do projeto
+  // configuração para rodar testes paralelos
   projects: [
     {
-      name: 'Chromium',
+      name: 'chromium',  // navegador Chrome
       use: { browserName: 'chromium' },
     },
-    {
-      name: 'Firefox',
-      use: { browserName: 'firefox' },
-    },
-    {
-      name: 'Webkit',
-      use: { browserName: 'webkit' },
-    },
   ],
-  retries: 1, // Número de tentativas em caso de falha de um teste
-  timeout: 30000, // Tempo máximo de execução por teste em milissegundos
-  reporter: [['list'], ['html']], // Relatórios de teste
+  // tempos limite
+  timeout: 60000,
 });
